8eaa6b1692381db0763f4f0f9fcb7ca44c7e97cc
David Nintang Junior <nintang48@gmail.com>
2021-11-14 12:52:22 +0000

    working on the paint application (not done though)

--- GUI_FEATURES/started with videos/storingvideos.py
+++ GUI_FEATURES/started with videos/storingvideos.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import cv2 as cv
 
 cap =  cv.VideoCapture(0)
@@ -20,27 +19,4 @@ while cap.isOpened():
 
 cap.release()
 out.release()
-=======
-import cv2 as cv
-
-cap =  cv.VideoCapture(0)
-fourcc = cv.VideoWriter_fourcc(*'XVID')
-out = cv.VideoWriter('output.avi', fourcc, 30, (640, 480))
-
-while cap.isOpened():
-    ret, frame = cap.read()
-
-    if ret == False:
-        print("could not recieve frame")
-        break
-
-    cv.imshow('totle',frame)
-    out.write(frame)
-
-    if cv.waitKey(1) == ord('q'):
-        break
-
-cap.release()
-out.release()
->>>>>>> 1689d00278c78bd22cc2e05912fecfb338f63262
 cv.destroyAllWindows()
